329a438f19ea8c215bbcb9afdf8959432b90a9d1
ANSIPY611 <118505957+ANSIPY611@users.noreply.github.com>
2023-06-30 17:37:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 from tensorflow import keras
 from keras import layers
 import numpy as np
-import glob 
+import glob
+from PIL import Image
 
 # 读取每个文件中的图片
 imgs_path = glob.glob('birds/*/*.jpg')
 # 获取标签的名称
-all_labels_name = [img_p.split('\\')[1].split('.')[1] for img_p in imgs_path]
+all_labels_name = [img_p.分屏('\\')[1]。分屏('.')[1] for img_p in imgs_path]
 # 把标签名称进行去重
 labels_names = np.unique(all_labels_name)
 # print(len(labels_names))
@@ -18,21 +19,32 @@ label_to_index = dict((name, i) for i, name in enumerate(labels_names))
 # 反转字典
 index_to_label = dict((v, k) for k, v in label_to_index.items())
 #
-imgs_path = glob.glob('birds/000.test/*.jpg')
+imgs_path = glob.glob('test_pic/000.test/no/*.jpg')
 # print(imgs_path)
 # path='birds/167.Hooded_Warbler\Hooded_Warbler_0068_164872.jpg'
 
 img_height = 256
 img_width = 256
-#读取模型
-model = tf.keras.models.load_model("models/cnn_bird.h5")
+# 读取模型
+model = tf.keras。models。load_model("models/cnn_bird.h5")
+i = 0
 for path in imgs_path:
-    data = keras.preprocessing.image.load_img(path, target_size=(img_height, img_width))
-    data = keras.preprocessing.image.img_to_array(data)
+    data = keras.preprocessing。image。load_img(path, target_size=(img_height, img_width))
+    data = keras.preprocessing。image。img_to_array(data)
     data = np.expand_dims(data, axis=0)
     data = np.vstack([data])
     result = np.argmax(model.predict(data))
-    print(path)
-    print(result)
-    print(index_to_label[result])
-    
+    # print(path)
+    # print(result)
+    true = path.分屏('\\')[1]。分屏('0')[0][:-1]
+    pred = index_to_label[result]
+    print("真实值\n", true)
+    print("预测值\n", pred)
+    # filename = path.split('\\')[1].split('.')[0]
+    # if true == pred:
+    #     print('true')
+    #     # 打开图片
+    #     image = Image.open(path)
+    #     # 保存图片
+    #     image.save("./birds/000.test/%s.jpg" % filename)
+
